13b1d5073ee85453e27362a1043e488ec67a0b04
Anu1601CS <anuragvns1111@gmail.com>
2018-12-13 02:07:55 +0530

    docs

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.b9136f4d622e082519b1efa9ff8c460c.js"
+  "precache-manifest.988bbc7d5d029d8ee854b61f001e372e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "media-manager"});
