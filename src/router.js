ba1eee363a8898950d34fd58a1ed2f2f20df4fe1
Anu1601CS <anuragvns1111@gmail.com>
2018-12-25 09:56:06 +0530

    pdf

--- src/router.js
+++ src/router.js
@@ -29,12 +29,12 @@ export default new Router({
             component: () => import(/* webpackChunkName: "about" */ './views/Signup.vue')
         },
         {
-            path: '/pdf',
-            name: 'pdf',
+            path: '/test',
+            name: 'test',
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ './views/Pdf.vue')
+            component: () => import(/* webpackChunkName: "about" */ './views/Test.vue')
         }
     ]
 })
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,3 @@
-const output = {
-    globalObject: "this",
-};
-
-const publicPath = process.env.WEBPACK_PUBLIC_PATH;
-if (publicPath) {
-    Object.assign(output, {publicPath});
-}
-
 module.exports = {
     baseUrl: "",
     pwa: {
@@ -14,19 +5,5 @@ module.exports = {
     },
     devServer: {
         proxy: "http://localhost:3344"
-    },
-    configureWebpack: {
-        devServer: {
-            watchOptions: {
-                poll: true,
-            },
-        },
-        output,
-    },
-    chainWebpack: config => {
-        config.module
-        .rule('svg')
-        .use('file-loader')
-        .loader('vue-svg-loader')
-    },
+    }
 };
