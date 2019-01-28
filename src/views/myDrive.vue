6086e2d144126bc225aed3a4fe94181a79602383
Anu1601CS <anuragvns1111@gmail.com>
2019-01-29 01:21:26 +0530

    new

--- src/views/myDrive.vue
+++ src/views/myDrive.vue
@@ -113,12 +113,6 @@ export default {
       this.findDisk(to, from);
       this.$store.commit(types.UNSELECT_ALL_BROWSER_ITEMS);
       this.$store.state.loadLimit = 30;
-      this.$store.state.contents = [];
-      if (to.params.dir) {
-        this.$store.dispatch("getContents", { path: to.params.dir });
-      } else {
-        this.$store.dispatch("getContents", { path: "my-drive" });
-      }
     }
   }
 };
