eaf9dea2ea046bb32d3734b009546fe000ed1e48
anu1601cs <anuragvns1111@gmail.com>
2019-06-11 23:26:37 +0530

    loopback 3

---
+++ server/server/boot/authentication.js
@@ -0,0 +1,11 @@
+// Copyright IBM Corp. 2016. All Rights Reserved.
+// Node module: loopback-workspace
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+
+'use strict';
+
+module.exports = function enableAuthentication(server) {
+  // enable authentication
+  server.enableAuth();
+};
